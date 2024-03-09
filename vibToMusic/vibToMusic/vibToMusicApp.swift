20ceeb7fe14bac3c067a1464a564b1c7ffce0db9
Aadi Sharma <aadisharma0226@gmail.com>
2024-03-09 10:13:52 -0800

    added Xcode

---
+++ vibToMusic/vibToMusic/vibToMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibToMusicApp.swift
+//  vibToMusic
+//
+//  Created by Aadi Sharma on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct vibToMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
